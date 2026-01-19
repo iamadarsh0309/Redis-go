66e636c3fcf27d3345af0b42da31539f7b12f923
Adarsh Pandey <adarshpandeyy00@gmail.com>
2026-01-20 01:16:24 +0530

    Add support for configuration file

---
+++ conf.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Config struct {
+	dir       string
+	rdb       []RDBSnapshot
+	rdbFn     string
+	aofEnabled bool
+	aoFfn     string
+	aofFsync  FSyncMode
+}
+
+func NewConfig() *Config {
+	return &Config{}
+}
+
+type RDBSnapshot struct {
+	Secs        int
+	KeysChanged int
+}
+
+type FSyncMode string
+
+const (
+	Always   FSyncMode = "always"
+	EverySec FSyncMode = "everysec"
+	No       FSyncMode = "no"
+)
+
+func readConf(fn string) *Config {
+	conf := NewConfig()
+	f, err := os.Open(fn)
+	if err!=nil{
+		fmt.Printf("Cannot read %s- using default config\n",fn)
+		return conf
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		l:= s.Text()
+		parseLine(l,conf)
+	}
+
+	if err := s.Err(); err!=nil {
+		fmt.Println("error scanning config file",err)
+		return conf
+	}
+
+	if conf.dir != ""{
+		os.MkdirAll(conf.dir,0755)
+	}
+	return conf
+
+}
+
+func parseLine(l string, conf *Config) {
+	args := strings.Split(l, " ")
+	cmd := args[0]
+
+	switch cmd{
+	case "save" :
+		secs,err := strconv.Atoi(args[1])
+		if err!=nil{
+			fmt.Println("invalid seconds")
+			return
+		}
+		keysChanged,err := strconv.Atoi(args[2])
+		if err!=nil{
+			fmt.Println("invalid keys")
+			return
+		}
+
+		snapshot := RDBSnapshot{
+			Secs: secs,
+			KeysChanged: keysChanged,
+		}
+		conf.rdb = append(conf.rdb, snapshot)
+
+	case "dbfilename" :
+		conf.rdbFn = args[1]
+	case "appendfilename" :
+		conf.aoFfn = args[1]
+	case "appendfsync" :
+		conf.aofFsync = FSyncMode(args[1])
+	case "dir" :
+		conf.dir = args[1]
+	case "appendonly" :
+		if args[1] == "yes" {
+			conf.aofEnabled = true
+		}else{
+			conf.aofEnabled = false
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,135 +1,38 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
-	"strconv"
 )
-
-type ValueType string
-
-const (
-	ARRAY ValueType = "*"
-	BULK ValueType = "$"
-	STRING ValueType = "+"
-	ERROR ValueType = "-"
-	NULL ValueType = ""
-)
-
-type Value struct {
-	typ ValueType
-	bulk string
-	str string
-	err string
-	array []Value
-}
-
-func (v *Value) readArray(reader io.Reader){
-	buf:= make([]byte,4)
-	reader.Read(buf)
-	arrLen, err := strconv.Atoi(string(buf[1]))
-	if err!=nil {
-		fmt.Println(err)
-		return
-	}
-
-	for range arrLen {
-		bulk := v.readBulk(reader)
-		v.array = append(v.array, bulk)
-	}
-
-}
-
-func (v *Value) readBulk (reader io.Reader) Value {
-	buf := make([]byte, 4)
-	reader.Read(buf)
-
-	n,err := strconv.Atoi(string(buf[1]))
-	if err!=nil {
-		fmt.Println(err)
-		return Value{}
-
-	}
-
-	bulkBuf := make([]byte, n+2)
-	reader.Read(bulkBuf)
-
-	bulk := string(bulkBuf[:n])
-	return Value{typ: BULK, bulk:bulk}
-
-}
-
 
 func main() {
+	log.Println("Reading config file")
+	readConf("./redis.conf")
+
 	ln, err := net.Listen("tcp", ":6379")
 
-	if err!=nil {
+	if err != nil {
 		log.Fatal("Cannot listen on port : 6379")
 	}
 	defer ln.Close()
-
+	log.Println("Listening on 6379")
 	conn, err := ln.Accept()
-	if err!=nil {
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	defer conn.Close()
 
 	for {
-	v:= Value{typ: "ARRAY"} //RESP Protocol always sends an array
-	v.readArray(conn)
+		v := Value{typ: "ARRAY"} //RESP Protocol always sends an array
+		v.readArray(conn)
 
+		handle(conn, &v)
 
-	fmt.Println(v.array)
+		fmt.Println(v.array)
 
-
-	conn.Write([]byte("+OK\r\n"))
-
-}
-
-}
-
-type Handler func(*Value) *Value
-
-var Handlers = map[string]Handler{}
-
-func handle(conn net.Conn, v *Value){
-	cmd := v.array[0].bulk
-	handler, ok := Handlers[cmd]
-	if !ok {
-		fmt.Println("invalid command")
-		return
-	}
-	reply :=  handler(v)
-	w:= NewWriter(conn)
-	w.Write(reply)
-
-}
-
-type Writer struct {
-	writer io.Writer
-}
-
-func NewWriter(w io.Writer) *Writer {
-	return &Writer{writer: bufio.NewWriter(w)}
-}
-
-func (w *Writer) Write(v *Value) {
-	var reply string
-	switch v.typ {
-	case STRING:
-		reply= fmt.Sprintf("%s%s\r\n", v.typ, v.str)
-	case BULK:
-		reply = fmt.Sprintf("%s%d\r\n%s\r\n", v.typ, len(v.bulk), v.bulk)
-	case ERROR:
-		reply=fmt.Sprintf("%s%s\r\n", v.typ, v.err)
-	case NULL:
-		reply=fmt.Sprintf("$-1\r\n")
 	}
 
-	w.writer.Write([]byte(reply))
 }
